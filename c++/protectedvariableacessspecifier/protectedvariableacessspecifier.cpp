5c2e4818dfa842d9a15c19cfba4dc6a7db8e89f6
yog <yogitsingh16@gmail.com>
2021-05-27 19:01:26 +0530

    public inhetitace can transef all public member as public and protected member as protected

---
+++ c++/protectedvariableacessspecifier/protectedvariableacessspecifier.cpp
@@ -0,0 +1,100 @@
+/*
+ * protectedacessspecifier.cpp
+ *
+ *  Created on: 26 May 2021
+ *      Author: yo
+ */
+#include <iostream>
+using namespace std;
+
+class base{
+protected:
+	int i,j;
+public:
+	void set(int x, int y)
+	{
+		i=x;
+		j=y;
+	}
+	void show()
+	{
+		cout<<"\n value of i"<<i;
+		cout<<"\n value of j"<<j;
+	}
+};
+class derived1: public base{
+	int k;
+public:
+	void setadd()
+	{
+		k=i+j;
+	}
+	void showadd()
+	{
+		cout<<"\n value od addition"<<k;
+	}
+
+};
+
+class derived2: public derived1{
+	int o;
+public:
+	void setmin(){
+		o=i-j;
+	}
+	void showmin()
+	{
+		cout<<"\n value od sub"<<o;
+	}
+};
+/*
+ * class derived2: public derived1{
+	int o;
+protected:
+	int i,j;
+public:
+
+	void set(int x, int y)
+	{
+		i=x;
+		j=y;
+	}
+	void show()
+	{
+		cout<<"\n value of i"<<i;
+		cout<<"\n value of j"<<j;
+	}
+
+	void setadd()
+	{
+		k=i+j;
+	}
+	void showadd()
+	{
+		cout<<"\n value od addition"<<k;
+	}
+
+	void setmin(){
+		o=i-j;
+	}
+	void showmin()
+	{
+		cout<<"\n value od sub"<<o;
+	}
+};
+ */
+int main(int argc, char **argv) {
+
+	derived2 ob2;
+	ob2.set(5, 5);
+	ob2.show();
+
+	ob2.setadd();
+	ob2.showadd();
+
+	ob2.setmin();
+	ob2.showmin();
+
+	return 0;
+}
+
