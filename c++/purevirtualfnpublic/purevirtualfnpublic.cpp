417ea1ef32755d7a72b17f04eb35a2c090cc5ff1
yog <yogitsingh16@gmail.com>
2021-06-10 18:39:27 +0530

    pure virtual

--- c++/purevirtualfnpublic/purevirtualfnpublic.cpp
+++ c++/purevirtualfnpublic/purevirtualfnpublic.cpp
@@ -4,6 +4,18 @@
  *  Created on: 9 Jun 2021
  *      Author: yo
  */
+/*
+ * 1. However, in many situations there can be no meaningful definition of a virtual
+function within a base class. For example, a base class may not be able to define
+an object sufficiently to allow a base-class virtual function to be created. Further,
+in some situations you will want to ensure that all derived classes override a virtual
+function. To handle these two cases, C++ supports the pure virtual function.
+
+2.Keep in mind that when a virtual function is declared as pure, all derived classesmust override it.
+3.A pure virtual function is a virtual function that has no definition within the base
+class. To declare a pure virtual function, use this general form:
+virtual type func-name(parameter-list) = 0;
+ */
 #include <iostream>
 using namespace std;
 
@@ -62,3 +74,4 @@ int main()
 	return 0;
 }
 
+//Although you cannot create objects of an abstract class, you can create pointers and references to an abstract class.
