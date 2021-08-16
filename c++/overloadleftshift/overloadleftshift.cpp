228fe1d145b8f7dd11111860e2ac5bf22a812b95
yog <yogitsingh16@gmail.com>
2021-08-16 18:24:12 +0530

    overloaded leftshift

---
+++ c++/overloadleftshift/overloadleftshift.cpp
@@ -0,0 +1,41 @@
+/*
+ * overload<<.cpp
+ *
+ *  Created on: 13 Aug 2021
+ *      Author: yo
+ */
+//friend is not member function of class
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+class over{
+	char name[20];
+	int id;
+public:
+	over(char *n,int i)
+	{
+		memset(name,0,sizeof(name));
+		memcpy(name,n,strlen(n));
+		id=i;
+	}
+	friend ostream &operator<<(ostream &s,over o);
+};
+
+ostream &operator<<(ostream &s,over o) //overloaded stream
+{
+	s<<"name is:"<<o.name;
+	s<<"\n"<<"id"<<o.id;
+
+	return s;
+}
+int main()
+{
+	over ob("ram",1),ob2("sham",2);
+	cout<<ob<<"\n"<<ob2;
+
+	return 0;
+}
